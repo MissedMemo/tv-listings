e34dbc23677650c6a80a34ac45d32c9133cf99aa
AndyL <missedmemo@gmail.com>
2016-09-25 12:23:23 -0700

    tweak output text to verify config changes work on Heroku

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import {render} from 'react-dom';
 import App from './components/app';
 
 render(
-  <App greeting='Hey, React!' />,
+  <App greeting='Hello, React!' />,
   document.getElementById('root')
 );
