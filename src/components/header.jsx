d0ccbc2eae4cfacdef3c0489f645003918ccfadb
AndyL <missedmemo@gmail.com>
2016-09-30 12:42:41 -0700

    reference color scheme values & begin responsive layout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,17 @@ import classnames from 'classnames/bind';
 import styles from './header.css'; 
 
 export default () => <div className={ styles.header }>
-  <div className={ styles.logo }>
-    What's<em>On!</em>
+  <div className={ styles.attention }>
+    <div className={ styles.logo }>
+      What's<em>On!</em>
+    </div>
+    <div className={ styles.tagline }>
+      "The best TV listings guide for the interwebs!"
+    </div>
+    <div className={ styles.infobar }>
+      <a href="#" className={ styles.infolink }>login</a>
+      &nbsp;&nbsp;&#8226;&nbsp;&nbsp;
+      <a href="#" className={ styles.infolink }>about</a>
+    </div>
   </div>
 </div>;
