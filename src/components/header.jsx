631bbeb7dbb7d5c5aca601d13b7cf395f01df4cf
AndyL <missedmemo@gmail.com>
2016-09-30 14:56:59 -0700

    add navbar to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import classnames from 'classnames/bind';
+//import classnames from 'classnames/bind';
+import NavBar from './navbar';
 import styles from './header.css'; 
 
 export default () => <div className={ styles.header }>
-  <div className={ styles.attention }>
+  <div className={ styles.row1 }>
     <div className={ styles.logo }>
       What's<em>On!</em>
     </div>
@@ -15,5 +16,8 @@ export default () => <div className={ styles.header }>
       &nbsp;&nbsp;&#8226;&nbsp;&nbsp;
       <a href="#" className={ styles.infolink }>about</a>
     </div>
+  </div>
+  <div className={ styles.row2 }>
+    <NavBar />
   </div>
 </div>;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './navbar.css';
+
+export default () => <div className={ styles.navbar }>
+  <a href="#" className={ styles.navlink }>programs</a>
+  <a href="#" className={ styles.navlink }>my schedule</a>
+  <a href="#" className={ styles.navlink }>profile</a>
+</div>
