3b03c2a0b35a3eeff2d5fcc685680dc9ad5e7645
AndyL <missedmemo@gmail.com>
2016-10-01 14:42:23 -0700

    refactor layout toward responsive design & improved appearance

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,6 +7,12 @@ import styles from './header.css';
 export default () => <div className={ styles.header }>
 
   <div className={ styles.row1 }>
+    <a href="#" className={ styles.infolink }>login</a>
+    &nbsp;&nbsp;&#8226;&nbsp;&nbsp;
+    <a href="#" className={ styles.infolink }>about</a>
+  </div>
+
+  <div className={ styles.row2 }>
 
     <div className={ styles.logo }>
       What's<em>On!</em>
@@ -16,20 +22,14 @@ export default () => <div className={ styles.header }>
       "The best TV listings guide for the interwebs!"
     </div>
 
-    <div className={ styles.infobar }>
-      <a href="#" className={ styles.infolink }>login</a>
-      &nbsp;&nbsp;&#8226;&nbsp;&nbsp;
-      <a href="#" className={ styles.infolink }>about</a>
-    </div>
-
   </div>
 
-  <div className={ styles.row2 }>
-    <NavBar />
-  </div>
-
-  <div className={ styles.searchbar } >
+  <div className={ styles.row3 }>
     <SearchBar />
+  </div>
+
+  <div className={ styles.row4 }>
+    <NavBar />
   </div>
 
 </div>;
--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './searchbar.css';
 
-export default () => <form className={ styles.searchbar }>
-  <input type="text" placeholder="find all the stuffs..."/>
-  <button type="submit">
-    <div className={ styles.magnifyingglass }>
-    </div>
-  </button>
-</form>
+export default () => <div className={ styles.searchbar }>
+  <form>
+    <input type="text" placeholder="find all the stuffs..."/>
+    <button type="submit">
+      <div className={ styles.magnifyingglass }>
+      </div>
+    </button>
+  </form>
+</div>
