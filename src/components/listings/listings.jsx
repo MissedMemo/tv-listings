56c89f58ef292caaed3332b161f08a007ce77934
AndyL <missedmemo@gmail.com>
2016-10-06 15:49:13 -0700

    test ajax call (axios) to listings API and fill list

--- src/components/listings/listings.jsx
+++ src/components/listings/listings.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios'
 import FilterBar from './filterbar';
 import styles from './listings.css';
 
-export default () => <div className={ styles.listings }>
-  <FilterBar />
-  <div className={ styles.message }>
-    <h3>this is our default LISTINGS view...</h3>
-  </div>
-</div>;
+export default class Listings extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      listings: []
+    }
+  }
+
+  componentWillMount() {
+    axios.get(`http://api.tvmaze.com/schedule?country=US&date=2016-10-07&page=1`)
+      .then( res => {
+        const listings = res.data.map( d =>({
+          title: d.show.name,
+          description: d.show.summary
+        }) );
+        this.setState({ listings });
+      });
+  }
+
+  render() {
+    return <div className={ styles.listings }>
+      <FilterBar />
+      <div>
+        <ul>
+          { this.state.listings.map( show => <li>
+            <div className={ styles.program }>
+              <div className={ styles.title }>{ show.title }</div>
+              <div className={ styles.desc }>{ show.description }</div>
+            </div>
+          </li>)}
+        </ul>
+      </div>
+    </div>;
+  }
+  
+};
