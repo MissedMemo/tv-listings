2900142ffd6d44d1304c62172e7662e6211b34b3
AndyL <missedmemo@gmail.com>
2016-10-06 18:13:47 -0700

    begin program (list item) element layout

--- src/components/listings/listings.jsx
+++ src/components/listings/listings.jsx
@@ -15,8 +15,13 @@ export default class Listings extends Component {
   componentWillMount() {
     axios.get(`http://api.tvmaze.com/schedule?country=US&date=2016-10-07&page=1`)
       .then( res => {
+        /*
+        console.log( 'record:', res.data[0], '(of #):', res.data.length );
+        */
         const listings = res.data.map( d =>({
+          id: d.id,
           title: d.show.name,
+          image: d.show.image ? d.show.image.medium || null : null,
           description: d.show.summary
         }) );
         this.setState({ listings });
@@ -28,10 +33,17 @@ export default class Listings extends Component {
       <FilterBar />
       <div>
         <ul>
-          { this.state.listings.map( show => <li>
+          { this.state.listings.map( show => <li key={show.id}>
             <div className={ styles.program }>
-              <div className={ styles.title }>{ show.title }</div>
-              <div className={ styles.desc }>{ show.description }</div>
+              <div className={ styles.image } data-image={ show.image } />
+              <div className={ styles.info }>
+                <div className={ styles.row1 }>
+                  <div className={ styles.title }>{ show.title }</div>
+                </div>
+                <div className={ styles.desc }>{ show.description }</div>
+                <div className={ styles.row3 }>
+                </div>
+              </div>
             </div>
           </li>)}
         </ul>
