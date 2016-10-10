db174d621b9862deedc399491da8e81c7d604d36
AndyL <missedmemo@gmail.com>
2016-10-09 18:25:48 -0700

    restyle and dynamically initialize genre and type droplists

--- src/components/listings/filterbar.jsx
+++ src/components/listings/filterbar.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import styles from './filterbar.css';
 
+
 export default () => <div className={ styles.filterbar }>
   <button>
     <div className={ styles.triangleleft }/>
   </button>
   <div className={ styles.filtersection }>
-    <select>
-      <option value="all-types">All Program Types</option>
-      <option value="documentaries">Documentaries</option>
-      <option value="films">Films</option>
-      <option value="sports">Sporting Events</option>
-    </select>
-    <select>
-      <option value="all-genres">All Genres</option>
-      <option value="comedy">Comedy</option>
-      <option value="drama">Drama</option>
-      <option value="science">Science</option>
-      <option value="politics">Politics</option>
-    </select>
+    <select>{ genres.map(
+      genre => <option value={ genre.value }>{ genre.text }</option>
+    )}</select>
+    <select>{ types.map(
+      type => <option value={ type.value }>{ type.text }</option>
+    )}</select>
   </div>
   <div className={ styles.filtersection }>
     <input type='date' />
@@ -28,3 +22,23 @@ export default () => <div className={ styles.filterbar }>
     <div className={ styles.triangleright }/>
   </button>
 </div>;
+
+
+{/* we can pass options via props for testing etc. */}
+
+const genres = [
+  { text: 'All Genres', value: 'all'     },
+  { text: 'Comedy',     value: 'comedy'  },
+  { text: 'Drama',      value: 'drama'   },
+  { text: 'Science',    value: 'science' },
+  { text: 'Sci-Fi',     value: 'sci-fi'  },
+  { text: 'Horror',     value: 'horror'  }
+];
+
+const types = [
+  { text: 'Any Program Type', value: 'any'            },
+  { text: 'Movie',            value: 'movie'          },
+  { text: 'Talk Show',        value: 'talk-show'      },
+  { text: 'Documentary',      value: 'documentary'    },
+  { text: 'Sporting Event',   value: 'sporting-event' }
+];
